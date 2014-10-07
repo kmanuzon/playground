6a51bd50cef5ddabcf79026cab4d0251e486be23
progknife <progknife@users.noreply.github.com>
2014-10-07 05:41:33 -0400

    test #8

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php
-// test only #7
-// b1948c34668d1e816e2d8b51936951342ac36468
-
-$p1 = file_get_contents('payload1.txt');
-$p2 = file_get_contents('payload2.txt');
-
-print hash_hmac('sha1', $p1, 'karlitoswaydevel');
-print '<br>';
-print hash_hmac('sha1', $p2, 'karlitoswaydevel');
+// test only #8
